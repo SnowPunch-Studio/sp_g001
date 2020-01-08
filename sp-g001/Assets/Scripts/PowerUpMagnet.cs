5b9fd08b336b1bc9fe76972933112c162c922f25
Ahmed Boulad <ahmedboulad95@gmail.com>
2020-01-07 20:53:29 -0600

    Updated shield powerup to destroy all surrounding obstacles and added magnet powerup

---
+++ sp-g001/Assets/Scripts/PowerUpMagnet.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpMagnet : PowerUp
+{
+    bool isActive;
+    HashSet<GameObject> foundGameObjects = new HashSet<GameObject>();
+    float speed = 2f;
+    bool m_Started;
+    Vector3 sphereCenter;
+    float sphereRadius;
+
+    protected override void Start()
+    {
+        base.Start();
+        m_Started = true;
+    }
+
+    protected override void PowerUpPayload()
+    {
+        base.PowerUpPayload();
+
+        isActive = true;
+        StartCoroutine(Finish());
+    }
+
+    private void Update()
+    {
+        if(isActive)
+        {
+            Debug.Log("Is Active");
+            //sphereCenter = new Vector3(playerController.transform.position.x, playerController.transform.position.y, playerController.transform.position.z + 2);
+            //sphereRadius = 3f;
+
+            Collider[] hitColliders = Physics.OverlapSphere(sphereCenter, sphereRadius);
+            foreach(Collider col in hitColliders)
+            {
+                if(col.gameObject.tag == "Coin")
+                {
+                    Debug.Log("Found Coin");
+                    if (foundGameObjects.Add(col.gameObject))
+                    {
+                        Debug.Log("Coin is new");
+                        float step = speed * Time.deltaTime;
+                        col.gameObject.transform.position = Vector3.MoveTowards(col.gameObject.transform.position, playerController.gameObject.transform.position, step);
+                    }
+
+                    if(col.gameObject.transform.position == playerController.gameObject.transform.position)
+                    {
+                        Debug.Log("Coin made it to player");
+                        // Increment player coin count
+                        Destroy(col.gameObject);
+                    }
+
+                    Debug.Log("HashSet :: " + foundGameObjects);
+                }
+            }
+        }
+    }
+
+    IEnumerator Finish()
+    {
+        yield return new WaitForSeconds(10);
+
+        PowerUpHasExpired();
+    }
+
+    protected override void PowerUpHasExpired()
+    {
+        playerController.SetPowerUpState(PlayerController.PowerUpState.NONE);
+        base.PowerUpHasExpired();
+    }
+
+    void OnDrawGizmos()
+    {
+        sphereCenter = new Vector3(playerController.transform.position.x, playerController.transform.position.y, playerController.transform.position.z + 2);
+        sphereRadius = 3f;
+
+        Gizmos.color = Color.cyan;
+        if (m_Started && playerController != null)
+            Gizmos.DrawWireSphere(sphereCenter, sphereRadius);
+    }
+}
--- sp-g001/Assets/Scripts/PowerUpShield.cs
+++ sp-g001/Assets/Scripts/PowerUpShield.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class PowerUpShield : PowerUp, IPlayerEvents
 {
+    bool m_Started;
+    Vector3 boxScale;
+    Vector3 boxCenter;
+
+    protected override void Start()
+    {
+        base.Start();
+        m_Started = true;
+    }
+
     protected override void PowerUpPayload()
     {
         base.PowerUpPayload();
@@ -25,9 +35,30 @@ public class PowerUpShield : PowerUp, IPlayerEvents
 
         if(other.gameObject.tag == "BoxObstacle")
         {
-            Destroy(other.gameObject);
+            Collider[] hitColliders = Physics.OverlapBox(boxCenter, boxScale, Quaternion.identity);
+
+            foreach(Collider col in hitColliders)
+            {
+                if(col.gameObject.tag == "BoxObstacle")
+                {
+                    Destroy(col.gameObject);
+                }
+            }
+
+            //Destroy(other.gameObject);
             PowerUpHasExpired();
         }
+    }
+
+    void OnDrawGizmos()
+    {
+        boxCenter = new Vector3(playerController.transform.position.x, playerController.transform.position.y, playerController.transform.position.z + 4);
+        boxScale = playerController.transform.localScale * 6;
+        boxScale.z *= 2;
+
+        Gizmos.color = Color.cyan;
+        if(m_Started && playerController != null)
+            Gizmos.DrawWireCube(boxCenter, boxScale);
     }
 
     void IPlayerEvents.OnPlayerReachedExit(GameObject exit)
